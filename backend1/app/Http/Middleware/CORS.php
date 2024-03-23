b2200847f3253c7042ac439d513f3bbc8d085ccb
Aicha-Gaiech <aichagaiech@gmail.com>
2024-03-23 10:51:16 +0100

    Authenntication Project v1

---
+++ backend1/app/Http/Middleware/CORS.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept, X-Requested-With, charset, boundary, Content-Length'); // Autorise les en-têtes HTTP
+        header('Access-Control-Allow-Origin: *');
+        return $next($request);
+    }
+}
